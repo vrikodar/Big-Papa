95160cbeaa2edc763025864f7374c0a814d6632b
SxNade <73271976+SxNade@users.noreply.github.com>
2020-12-21 10:04:55 -0500

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,5 @@
+echo [*] Simple bash Script to install some python dependencies
+sudo apt install python3
+sudo apt install python3-pip
+sudo pip3 install termcolor
+echo [*] All Dependencies Successfully installed ....incase no error...!
